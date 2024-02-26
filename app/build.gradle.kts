a62557f8fdc43f6523c53a961525782f3ea5fd86
nat4files <nathaliadhy28@gmail.com>
2024-02-26 18:03:34 +0700

    menambahkan beberapa constrainlayout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.dev.myconstraintview"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.dev.myconstraintview"
